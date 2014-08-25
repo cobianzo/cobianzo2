5516fd16f83dbae808ce2db308c1b9c4ebb42141
Alvaro B <cobianzo@gmail.com>
2014-08-25 20:00:18 +0200

    including sass folder in the project

--- www/wp-content/themes/cobianzo2/functions.php
+++ www/wp-content/themes/cobianzo2/functions.php
@@ -92,12 +92,12 @@ add_action( 'widgets_init', 'cobianzo_widgets_init' );
  * Enqueue scripts and styles.
  */
 function cobianzo_scripts() {
-	wp_enqueue_style( 'cobianzo-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'cobianzo-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
+	wp_enqueue_style( 'cobianzo-style', get_template_directory_uri() . '/bootstrap/bootstrap.css' );
+	wp_enqueue_style( 'cobianzo-style', get_stylesheet_uri() );	
+	
+	
 	wp_enqueue_script( 'cobianzo-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
